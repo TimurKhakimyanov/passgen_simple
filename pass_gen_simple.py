2f99009fce0078b1bc37c070983c16650a1f702c
TimurKhakimyanov <35830474+TimurKhakimyanov@users.noreply.github.com>
2020-08-04 16:50:02 +0600

    Update pass_gen_simple.py

--- pass_gen_simple.py
+++ pass_gen_simple.py
@@ -1,5 +1,7 @@
+#The password generation program is very simple all letters are Latin
+#To use, just enter the desired password length and then the program will display the password
 #Программа для генерации пароля очень простая все буквы латинские
-#Для использования просто введите желаемую длину пароля далле программа выведет пароль
+#Для использования просто введите желаемую длину пароля далее программа выведет пароль
 #August  2020 
 from string import ascii_letters, ascii_lowercase, ascii_uppercase, digits
 from random import choice
