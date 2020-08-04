01d516b8083f9fa2b6e4f6d4b9ea0da354b8c4ae
TimurKhakimyanov <35830474+TimurKhakimyanov@users.noreply.github.com>
2020-08-04 16:28:56 +0600

    Update pass_gen_simple.py

--- pass_gen_simple.py
+++ pass_gen_simple.py
@@ -1,6 +1,6 @@
 #Программа для генерации пароля очень простая все буквы латинские
 #Для использования просто введите желаемую длину пароля далле программа выведет пароль
-#August 2, 2020 
+#August  2020 
 from string import ascii_letters, ascii_lowercase, ascii_uppercase, digits
 from random import choice
 chars = ascii_letters + digits
